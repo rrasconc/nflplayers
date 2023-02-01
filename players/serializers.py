673477bb191719516d01d44febe777f92fdaae11
Rigoberto Rascon <rigobertorascon5@gmail.com>
2023-02-01 09:45:07 -0700

    serialized player relationship fields

--- players/serializers.py
+++ players/serializers.py
@@ -2,12 +2,6 @@ from rest_framework import serializers
 from players.models import Player, Position, Team
 
 
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        fields = "__all__"
-
-
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
@@ -18,3 +12,14 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = "__all__"
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    position = serializers.CharField(source="position.name")
+    team = serializers.CharField(source="team.name")
+    conference = serializers.CharField(source="team.conference")
+    division = serializers.CharField(source="team.division")
+
+    class Meta:
+        model = Player
+        fields = ("first_name", "last_name", "number", "position", "team", "conference", "division")
